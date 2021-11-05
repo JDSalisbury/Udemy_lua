bdaac423bfa0c998a4592811cdcdb3eb88570f8f
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 20:54:57 -0400

    Ifs and stuff.

--- main.lua
+++ main.lua
@@ -1,5 +1,11 @@
 message = "Lua is awesome!"
 
+condition = 10
+
+if condition > 0 then
+    message = 1
+end
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(55))
     love.graphics.print(message)
