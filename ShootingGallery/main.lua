72c9a16f9cf0b4964400fb90bbdd27e2dc9c8d79
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 22:52:22 -0400

    Setting up Display Score

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -1,19 +1,29 @@
+-- Runs on Load, Start, Preliminary set up of sorts Global vars
 function love.load()
-    -- Runs on Load, Start, Preliminary set up of sorts Global vars
+
     target = {}
-    target.x = 0
-    target.y = 0
+    target.x = 200
+    target.y = 200
     target.radius = 50
+
+    SCORE = 0
+    TIMER = 0
+
+    gameStyles ={}
+    gameStyles.gameFont = love.graphics.newFont(40)
 end
 
+-- delta time shit, game loop shenanigans update Vars
 function love.update(dt)
-    -- delta time shit, game loop shenanigans update Vars
     
 end
 
+-- draw graphics tßo screen, also runs each frame like update, but this is just for graphics/images
 function love.draw()
-    -- draw graphics to screen, also runs each frame like update, but this is just for graphics/images
     
     love.graphics.setColor(1, 0, 0)
     love.graphics.circle('fill', target.x, target.y, target.radius)
+    love.graphics.setColor(1,1,1)
+    love.graphics.setFont(gameStyles.gameFont)
+    love.graphics.print(SCORE, 0, 0)
 end
