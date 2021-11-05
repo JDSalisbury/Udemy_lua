eb11bbb0367e2bde6e1154fed565878d17dec9b8
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 22:46:17 -0400

    Setting up target

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -1,6 +1,9 @@
 function love.load()
     -- Runs on Load, Start, Preliminary set up of sorts Global vars
-
+    target = {}
+    target.x = 0
+    target.y = 0
+    target.radius = 50
 end
 
 function love.update(dt)
@@ -11,8 +14,6 @@ end
 function love.draw()
     -- draw graphics to screen, also runs each frame like update, but this is just for graphics/images
     
-    love.graphics.setColor(1,0,1)
-    love.graphics.rectangle("fill", 0, 400, 200, 100)
-    love.graphics.setColor(1, 0.63, 0.4)
-    love.graphics.circle('fill', 300, 200, 100)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle('fill', target.x, target.y, target.radius)
 end
