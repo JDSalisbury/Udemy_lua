cdf675d05d48932b487cae2ce9ad35f878eaf6a1
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 21:24:17 -0400

    Functions in lua.

--- main.lua
+++ main.lua
@@ -1,25 +1,16 @@
 message = 1
 
-condition = -10
-
--- if condition > 0 then
---     message = 1
--- elseif condition == 0 then
---     message = "This is zero"
--- else
---     message = 'Something else'
--- end
-
-while message < 10 do
-    message = message +1
-end
-pickle = 0
-for i=1, 3, 1 do
-    pickle = pickle + 10
+function increaseMessage(num)
+    message = message + num
 end
 
+function double(num)
+    return num * 2
+end
+
+increaseMessage(12)
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(55))
-    love.graphics.print(pickle)
+    love.graphics.print(double(message))
 end
