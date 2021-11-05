eecaadf455a1c28f77d3c717ce679fefddd00f4b
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 21:28:45 -0400

    local vs global.

--- main.lua
+++ main.lua
@@ -1,16 +1,22 @@
-message = 1
+MESSAGE = 1
 
 function increaseMessage(num)
-    message = message + num
+    MESSAGE = MESSAGE + num
 end
 
 function double(num)
     return num * 2
 end
 
+function getHalf(num)
+    local var = num
+    return var / 2
+end
+
 increaseMessage(12)
+
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(55))
-    love.graphics.print(double(message))
+    love.graphics.print(getHalf(double(MESSAGE)))
 end
