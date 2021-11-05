fd9ab36846d1ec4c59168705d7e417077415707a
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 21:45:44 -0400

    table loops and dot notation

--- main.lua
+++ main.lua
@@ -4,10 +4,19 @@ testScores = {95, 87, 98}
 
 table.insert(testScores, 4, 100)
 
+testScores.subject = 'maths'
 
-MESSAGE = testScores
+
+for i, v in ipairs(testScores) do
+    if type(v) == 'number' then
+        MESSAGE = MESSAGE + v
+    end
+end
+
+
 
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(55))
-    love.graphics.print(MESSAGE)
+
+    love.graphics.print(testScores.subject)
 end
