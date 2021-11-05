348f5aee21a66739425701a96bbaf1c3b5135306
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 21:03:17 -0400

    While loop

--- main.lua
+++ main.lua
@@ -2,12 +2,16 @@ message = "Lua is awesome!"
 
 condition = -10
 
-if condition > 0 then
-    message = 1
-elseif condition == 0 then
-    message = "This is zero"
-else
-    message = 'Something else'
+-- if condition > 0 then
+--     message = 1
+-- elseif condition == 0 then
+--     message = "This is zero"
+-- else
+--     message = 'Something else'
+-- end
+
+while message < 10 do
+    message = message +1
 end
 
 function love.draw()
