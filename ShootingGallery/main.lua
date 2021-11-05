338bc9a108b4759bc450fcb053fb9a29b357b11b
Jeff <salisbury.jeffery@gmail.com>
2021-11-04 22:40:46 -0400

    Shapes and colors

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -1,16 +1,18 @@
 function love.load()
     -- Runs on Load, Start, Preliminary set up of sorts Global vars
 
-     NUM = 0
 end
 
 function love.update(dt)
     -- delta time shit, game loop shenanigans update Vars
-    NUM = NUM + 1
     
 end
 
 function love.draw()
     -- draw graphics to screen, also runs each frame like update, but this is just for graphics/images
-    love.graphics.print(NUM)
+    
+    love.graphics.setColor(1,0,1)
+    love.graphics.rectangle("fill", 0, 400, 200, 100)
+    love.graphics.setColor(1, 0.63, 0.4)
+    love.graphics.circle('fill', 300, 200, 100)
 end
